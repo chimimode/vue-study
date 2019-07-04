049bdef118a652d67f40ea803f7b075e81020cfe
lsj_0517 <lsj_0517@sbs.co.kr>
2019-07-04 18:12:08 +0900

    설정진행순서 확인 중

--- todolist/src/router/index.js
+++ todolist/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 Vue.use(Router)
 
 export default new Router({
