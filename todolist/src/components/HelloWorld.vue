eed77de31ce9deb0e14204f2bf691060e33ace91
lsj_0517 <lsj_0517@sbs.co.kr>
2019-07-16 11:04:18 +0900

    material세팅

--- todolist/src/components/HelloWorld.vue
+++ todolist/src/components/HelloWorld.vue
@@ -1,113 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="full-control">
+    <md-list>
+      <md-subheader>
+        <md-field>
+      <label>Type here!</label>
+      <md-input v-model="type"></md-input>
+      <span class="md-helper-text">새로등록</span>
+    </md-field>
+      </md-subheader>
+
+      <md-list-item>
+        <md-checkbox v-model="notification" value="preview" />
+        <span class="md-list-item-text">Show content preview</span>
+      </md-list-item>
+
+      <md-list-item>
+        <md-checkbox v-model="notification" value="sound" />
+        <span class="md-list-item-text">Sound</span>
+      </md-list-item>
+
+      <md-list-item>
+        <md-checkbox v-model="notification" value="vibrate" />
+        <span class="md-list-item-text">Vibrate</span>
+      </md-list-item>
+
+      <md-list-item>
+        <md-checkbox v-model="notification" value="light" />
+        <span class="md-list-item-text">Notification light</span>
+      </md-list-item>
+    </md-list>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'Controls',
+  data: () => ({
+    notification: ['sound', 'vibrate']
+  })
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  .full-control > .md-list {
+    width: 320px;
+    max-width: 100%;
+    height: 400px;
+    display: inline-block;
+    overflow: auto;
+    border: 1px solid rgba(#000, .12);
+    vertical-align: top;
+  }
 </style>
